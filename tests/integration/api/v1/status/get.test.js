c12451c80c6d7c43e82f0cb600c3abb4ea62497f
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2024-05-29 03:44:29 +0000

    test: return `updated_at` in `api/v1/status/` route in ISO 8601 format

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,8 @@
 test("GET on api/v1/status should return status 200", async () => {
   const response = await fetch("http:localhost:3000/api/v1/status");
-
+  const responseBody = await response.json()
+  console.log(responseBody)
+  const parsedResponseBody = new Date(responseBody.updated_at).toISOString()
+  expect(responseBody.updated_at).toBe(parsedResponseBody)
   expect(response.status).toBe(200)
 })
