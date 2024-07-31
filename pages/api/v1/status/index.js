e25acd01981c56263ea1baa2b4776db68c90d51c
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2024-07-31 02:58:50 +0000

    feat: finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,31 @@
 import database from 'infra/database';
 
 export default async function status(request, response) {
+  const { query } = database
   const updated_at = new Date().toISOString()
-  response.status(200).json({updated_at})
+
+  const dbVersion = await query('SHOW server_version;')
+  const dbVersionValue = dbVersion.rows[0].server_version
+
+  const maxConnections = await query('SHOW max_connections')
+  const maxConnectionsValue = parseInt(maxConnections.rows[0].max_connections)
+
+  const databaseName = process.env.POSTGRES_DB
+  const opennedConnections = await query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  })
+
+  const opennedConnectionsValue = opennedConnections.rows[0].count
+
+  response.status(200).json({
+    updated_at,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: maxConnectionsValue,
+        openned_connections: opennedConnectionsValue
+      }
+    },
+  })
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,6 +2,11 @@ test("GET on api/v1/status should return status 200", async () => {
   const response = await fetch("http:localhost:3000/api/v1/status");
   const responseBody = await response.json()
   const parsedResponseBody = new Date(responseBody.updated_at).toISOString()
+  const database = responseBody.dependencies.database
   expect(responseBody.updated_at).toBe(parsedResponseBody)
   expect(response.status).toBe(200)
+  expect(database.version).toBe("16.0")
+  expect(typeof Number(database.max_connections)).toBe('number')
+  console.log('openned_connections: ', database.openned_connections)
+  expect(Number(database.openned_connections) === 1).toBe(true)
 })
