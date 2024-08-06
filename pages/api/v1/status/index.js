bdc63d780451cf7765d6cc37b99e428d48bc07ec
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2024-08-06 03:33:34 +0000

    fix: change property name to `used_connections`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,6 +2,7 @@ import database from 'infra/database';
 
 export default async function status(request, response) {
   const { query } = database
+
   const updated_at = new Date().toISOString()
 
   const dbVersion = await query('SHOW server_version;')
@@ -10,13 +11,8 @@ export default async function status(request, response) {
   const maxConnections = await query('SHOW max_connections')
   const maxConnectionsValue = parseInt(maxConnections.rows[0].max_connections)
 
-  const databaseName = process.env.POSTGRES_DB
-  const opennedConnections = await query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName]
-  })
-
-  const opennedConnectionsValue = opennedConnections.rows[0].count
+  const connections = await query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'postgres'")
+  const connections_value = connections.rows[0].count
 
   response.status(200).json({
     updated_at,
@@ -24,7 +20,7 @@ export default async function status(request, response) {
       database: {
         version: dbVersionValue,
         max_connections: maxConnectionsValue,
-        openned_connections: opennedConnectionsValue
+        used_connections: connections_value
       }
     },
   })
