f71a9a5dcb3fdf6caff5cf3b27b3d2d44984efc0
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2025-01-20 03:39:49 +0000

    refactor: increase code structure and add POST return

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,26 +2,27 @@ import migrationRunner from 'node-pg-migrate';
 import { join } from 'node:path';
 
 export default async function migrations(request, response) {
+  const defaultMigrations = {
+    databaseUrl: process.env.DATABASE_URL,
+    dir: join('infra', 'migrations'),
+    direction: 'up',
+    verbose: true,
+    migrationsTable: 'pg-migrations'
+  };
+
   if (request.method === 'GET') {
     const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+      ...defaultMigrations,
       dryRun: true,
-      dir: join('infra', 'migrations'),
-      direction: 'up',
-      verbose: true,
-      migrationsTable: 'pg-migrations'
     });
     return response.status(200).json(migrations)
   }
   if (request.method === 'POST') {
     const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: false,
-      dir: join('infra', 'migrations'),
-      direction: 'up',
-      verbose: true,
-      migrationsTable: 'pg-migrations'
+      ...defaultMigrations,
+      dryRun: false
     });
+    if (migrations.length > 0) return response.status(201).json(migrations);
     return response.status(200).json(migrations)
   }
   return response.status(405).json({ message: 'method not allowed' })
