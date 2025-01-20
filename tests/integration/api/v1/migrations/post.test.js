dbaef5aa2cf57461fbf6292c14da0024247ff01b
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2025-01-20 03:40:18 +0000

    test: change POST return

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -7,10 +7,25 @@ async function cleanDatabase() {
 }
 
 test("POST to api/v1/migrations should return status 200", async () => {
-  const response = await fetch("http:localhost:3000/api/v1/migrations", {
+  const response1 = await fetch("http:localhost:3000/api/v1/migrations", {
     method: 'post'
   });
-  expect(response.status).toBe(200);
-  const responseBody = await response.json();
-  expect(Array.isArray(responseBody)).toBe(true);
+  expect(response1.status).toBe(201);
+
+  const response1Body = await response1.json();
+  expect(Array.isArray(response1Body)).toBe(true);
+
+  const migrations1 = await database.query('select * from "pg-migrations"');
+  expect(migrations1.rows.length).toBeGreaterThan(0);
+
+  const response2 = await fetch("http:localhost:3000/api/v1/migrations", {
+    method: 'post'
+  });
+  expect(response2.status).toBe(200);
+
+  const response2Body = await response2.json();
+  expect(Array.isArray(response2Body)).toBe(true);
+  expect(response2Body.length).toBe(0);
+  // const migrations2 = await database.query('select * from "pg-migrations"');
+  // expect(migrations2.rows.length).toBe(0);
 })
