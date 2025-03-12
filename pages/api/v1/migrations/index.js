61f5ced9b26f3d221255f782f4bd117714872500
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2025-03-12 02:50:52 +0000

    refactor: increase logic to allowed methods and close db client

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,34 +3,44 @@ import database from 'infra/database';
 import { join } from 'node:path';
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrations = {
-    dbClient: dbClient,
-    databaseUrl: process.env.DATABASE_URL,
-    dir: join('infra', 'migrations'),
-    direction: 'up',
-    verbose: true,
-    migrationsTable: 'pg-migrations'
-  };
+  const allowedMethods = ['GET', 'POST']
 
-  if (request.method === 'GET') {
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrations,
-      dryRun: true,
-    });
-    await dbClient.end()
-    return response.status(200).json(pendingMigrations)
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`
+    })
   }
+  let dbClient
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrations = {
+      dbClient: dbClient,
+      databaseUrl: process.env.DATABASE_URL,
+      dir: join('infra', 'migrations'),
+      direction: 'up',
+      verbose: true,
+      migrationsTable: 'pg-migrations'
+    };
 
-  if (request.method === 'POST') {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrations,
-      dryRun: false
-    });
+    if (request.method === 'GET') {
+      const pendingMigrations = await migrationRunner({
+        ...defaultMigrations,
+        dryRun: true,
+      });
+      return response.status(200).json(pendingMigrations)
+    }
+
+    if (request.method === 'POST') {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrations,
+        dryRun: false
+      });
+      if (migratedMigrations.length > 0) return response.status(201).json(migratedMigrations);
+      return response.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error)
+  } finally {
     await dbClient.end()
-    if (migratedMigrations.length > 0) return response.status(201).json(migratedMigrations);
-    return response.status(200).json(migratedMigrations);
   }
-  await dbClient.end()
-  return response.status(405).json({ message: 'method not allowed' })
 }
