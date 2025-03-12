1cf3197803d3527f0ed2c66e6b4d110065a83daa
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2025-03-12 04:02:04 +0000

    fix: correct database name on connections query

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -11,7 +11,11 @@ export default async function status(request, response) {
   const maxConnections = await query('SHOW max_connections')
   const maxConnectionsValue = parseInt(maxConnections.rows[0].max_connections)
 
-  const connections = await query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'postgres'")
+  const databaseName = process.env.POSTGRES_DB
+  const connections = await query({
+    text: 'SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1',
+    values: [databaseName]
+  })
   const connections_value = connections.rows[0].count
 
   response.status(200).json({
