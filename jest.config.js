fef1fb5ad3fa20a7d892267800a268d02ac5face
Leonardo <leonardolccabral@gmail.com>
2025-07-22 01:55:46 -0300

    chore: makes `npm test` more robust with `async-retry` and `orchestrator.js`

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,8 @@ const createJestConfig = nextJest({
   dir: '.'
 });
 const jestConfig = createJestConfig({
-  moduleDirectories: ['node_modules', '<rootDir>']
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  testTimeout: 60000
 });
 
 module.exports = jestConfig;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,3 +1,9 @@
+import orchestrator from 'tests/orchestrator.js';
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices()
+})
+
 test("GET on api/v1/status should return status 200", async () => {
   const response = await fetch("http:localhost:3000/api/v1/status");
   const responseBody = await response.json()
